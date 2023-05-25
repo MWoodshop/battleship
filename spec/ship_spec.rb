c72703a0285121547732fa57e4d20a6c6ae45c05
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-25 16:02:45 -0600

    it exists test

---
+++ spec/ship_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/ship'
+
+RSpec.describe Ship do
+    
+    it "exists" do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser).to be_an_instance_of(Ship)
+    end
+
+end
