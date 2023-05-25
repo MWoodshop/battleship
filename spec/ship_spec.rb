b7b5f403f53d7546a04ca7357181455dc38d5645
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-25 16:09:28 -0600

    Add Ship class with attributes

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Ship do
         expect(cruiser).to be_an_instance_of(Ship)
     end
 
+    it "has attributes" do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser.name).to eq("Cruiser")
+        expect(cruiser.length).to eq(3)
+        expect(cruiser.health).to eq(3)
+    end
+
+
 end
