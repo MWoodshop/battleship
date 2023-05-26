74024d6d3b72b25902446abbfb25a33c43d7b90b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-26 17:13:37 -0400

    feat: adds board grid to initialize on Board class

--- lib/board.rb
+++ lib/board.rb
@@ -5,5 +5,14 @@ class Board
 
   def initialize
     @cells = {}
+    rows = %w[A B C D]
+    columns = [1, 2, 3, 4]
+
+    rows.each do |row|
+      columns.each do |column|
+        coordinate = "#{row}#{column}"
+        @cells[coordinate] = Cell.new(coordinate)
+      end
+    end
   end
 end
