1f1206cdb1350ebd4fd8d4294db203387b3a45fd
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-26 17:44:15 -0400

    feat: add valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -19,4 +19,24 @@ class Board
   def valid_coordinate?(coordinate)
     @cells.keys.include?(coordinate)
   end
+
+  # Method tests is placement is valid based on several conditions
+  def valid_placement?(ship, coordinates)
+    # The coordinates argument from the test or player must be an array
+    return false unless coordinates.is_a?(Array)
+    # The ship argument must be Ship object
+    return false unless ship.is_a?(Ship)
+    # The coordinates length must be equal to the ship length
+    return false if coordinates.length != ship.length
+
+    # Iterate over each coordinate in the coordinates array
+    coordinates.each do |coordinate|
+      # Check if each coordinate is valid per valid_coordinate? method
+      return false unless valid_coordinate?(coordinate)
+      # Check that the cell object is not empty
+      return false unless @cells[coordinate].empty?
+    end
+    # If all of the above do not fail, return true
+    true
+  end
 end
