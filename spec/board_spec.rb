c19f1e3a720664ae9ffe20840437a9a5c3905808
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-26 17:18:03 -0400

    test: cells returns expected grid keys

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,14 +12,22 @@ RSpec.describe Board do
       board = Board.new
       expect(board).to be_a(Board)
     end
+
     it 'returns empty hash' do
       board = Board.new
       expect(board.cells).to be_a(Hash)
     end
+
     it 'returns a coordinate as a cell' do
       board = Board.new
 
       expect(board.cells['A1']).to be_a Cell
+    end
+
+    it 'returns the expected grid' do
+      board = Board.new
+
+      expect(board.cells.keys).to include('A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4')
     end
   end
 end
