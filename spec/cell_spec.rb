8d35a5502a6d477e2b54662dbdc220a66faa3ebb
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-25 22:00:09 -0400

    test: adds initialize object value test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,7 +4,12 @@ require './lib/cell'
 RSpec.describe Cell do
   describe '#initialize' do
     it 'exists' do
-      cell = Cell.new
+      cell = Cell.new('B4')
+    end
+    it 'returns correct value' do
+      cell = Cell.new('B4')
+
+      expect(cell.coordinate).to eq('B4')
     end
   end
 end
