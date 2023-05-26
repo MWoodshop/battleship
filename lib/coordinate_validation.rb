d326a78adb590c3c7f47ea9fbd30d8203ee3cf18
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-26 13:37:47 -0400

    fix: rewrote regex to account for both possible errors and added comments.

--- lib/coordinate_validation.rb
+++ lib/coordinate_validation.rb
@@ -8,16 +8,26 @@
 # 4 |_| |_| |_| |_|
 
 module CoordinateValidation
-  VALID_COORDINATE_REGEX = /^[A-D][1-4]$/
+  # This is a constant that defines the valid battleship coordinates using regex.
+  # The regex means that the coordinate must start with a letter A-D, and end with a number 1-4.
+  VALID_COORDINATE_REGEX = /^[A-Z]{1}\d{1,2}$/
 
+  # This method validates the coordinate format and checks if the coordinate is within the grid.
   def validate_coordinate(coordinate)
-    raise ArgumentError, 'Invalid coordinate format, coordinate must be one of these letters (A, B, C, D) and one of these numbers (1, 2, 3, 4)' unless coordinate.match?(VALID_COORDINATE_REGEX)
+    # The match? method returns true if the regex matches the coordinate string given as an argument.
+    # If the regex does not match the coordinate string given as an argument, the match? method returns false.
+    # If the match? method returns false, the following error is raised.
+    raise ArgumentError, 'Invalid coordinate format' unless coordinate.match?(VALID_COORDINATE_REGEX)
 
+    # This extracts the first character from the coordinate string as column
+    # then converts the rest of the coordinate string to an integer as row.
     column = coordinate[0]
     row = coordinate[1..-1].to_i
-
+    # This checks if the column is A-D and the row is 1-4.
+    # If the column is A-D and the row is 1-4, the method returns and no error is raised.
     return if ('A'..'D').include?(column) && (1..4).include?(row)
 
-    raise ArgumentError, 'Invalid coordinate, coordinate out of bounds.'
+    # If the column is not A-D and the row is not 1-4, the following error is raised.
+    raise ArgumentError, 'Invalid coordinate, coordinate out of bounds'
   end
 end
