50a649caa8dedf8a970430771e39a680211bba48
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-26 17:37:16 -0600

    add on to valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -28,15 +28,29 @@ class Board
     return false unless ship.is_a?(Ship)
     # The coordinates length must be equal to the ship length
     return false if coordinates.length != ship.length
+    # These two lines extract the row and column values from the coordinates array
+    rows = coordinates.map { |coord| coord[0] }
+    columns = coordinates.map { |coord| coord[1..].to_i }
 
-    # Iterate over each coordinate in the coordinates array
-    coordinates.each do |coordinate|
-      # Check if each coordinate is valid per valid_coordinate? method
-      return false unless valid_coordinate?(coordinate)
-      # Check that the cell object is not empty
-      return false unless @cells[coordinate].empty?
+    # Checks if the coordinates are consecutive in rows
+    if rows.uniq.length == 1 && (columns.min..columns.max).to_a == columns.sort
+      # If rows are the same and columns are consecutive
+      rows.all? do |row|
+        # Check each cell in the row is valid and empty
+        valid_coordinate = valid_coordinate?("#{row}#{columns.min}")
+        empty_cell = @cells["#{row}#{columns.min}"].empty?
+        valid_coordinate && empty_cell
+      end
+    elsif columns.uniq.length == 1 && (rows.min.ord..rows.max.ord).to_a == rows.map(&:ord).sort
+      # Checks if the coordinates are consecutive in columns
+      columns.all? do |column|
+        # Check each cell in the column is valid and empty
+        valid_coordinate = valid_coordinate?("#{rows.min}#{column}")
+        empty_cell = @cells["#{rows.min}#{column}"].empty?
+        valid_coordinate && empty_cell
+      end
+    else
+      false
     end
-    # If all of the above do not fail, return true
-    true
   end
 end
