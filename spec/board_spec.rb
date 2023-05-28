b01aab9957ce7c4fd6ce15b9604c2dd537281ca3
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-28 15:43:49 -0400

    test: add place method test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -90,22 +90,46 @@ RSpec.describe Board do
 
       expect(board.valid_placement?(submarine, [])).to eq(false)
     end
-    
+
     it 'test all ship placement' do
       board = Board.new
-      cruiser = Ship.new("Cruiser", 3)
-      submarine = Ship.new("Submarine", 2)
+      cruiser = Ship.new('Cruiser', 3)
+      submarine = Ship.new('Submarine', 2)
 
-      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
-      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
-      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
-      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+      expect(board.valid_placement?(cruiser, %w[A1 A2])).to eq(false)
+      expect(board.valid_placement?(submarine, %w[A2 A3 A4])).to eq(false)
+      expect(board.valid_placement?(cruiser, %w[A1 A2 A4])).to eq(false)
+      expect(board.valid_placement?(submarine, %w[A1 C1])).to eq(false)
+      expect(board.valid_placement?(cruiser, %w[A3 A2 A1])).to eq(false)
+      expect(board.valid_placement?(submarine, %w[C1 B1])).to eq(false)
+      expect(board.valid_placement?(cruiser, %w[A1 B2 C3])).to eq(false)
+      expect(board.valid_placement?(submarine, %w[C2 D3])).to eq(false)
+      expect(board.valid_placement?(submarine, %w[A1 A2])).to eq(true)
+      expect(board.valid_placement?(cruiser, %w[B1 C1 D1])).to eq(true)
+    end
+  end
+
+  # Iteration 2 - Placing Ships
+  describe '.place method' do
+    it '.ship returns correct ships' do
+      board = Board.new
+      cruiser = Ship.new('Cruiser', 3)
+      board.place(cruiser, %w[A1 A2 A3])
+      cell_1 = board.cells['A1']
+      cell_2 = board.cells['A2']
+      cell_3 = board.cells['A3']
+
+      expect(cell_1.ship.name).to eq('Cruiser')
+      expect(cell_1.ship.length).to eq(3)
+      expect(cell_1.ship.health).to eq(3)
+
+      expect(cell_2.ship.name).to eq('Cruiser')
+      expect(cell_2.ship.length).to eq(3)
+      expect(cell_2.ship.health).to eq(3)
+
+      expect(cell_3.ship.name).to eq('Cruiser')
+      expect(cell_3.ship.length).to eq(3)
+      expect(cell_3.ship.health).to eq(3)
     end
   end
 end
