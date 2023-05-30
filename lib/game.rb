328f4a01cee33f64de775c7ef6032dbc873f1864
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-05-30 17:31:13 -0400

    feat: can initialize and place cruiser and sub for player

--- lib/game.rb
+++ lib/game.rb
@@ -1,45 +1,80 @@
 require './lib/board'
+require './lib/ship'
+require './lib/cell'
+require 'pry'
 
 class Game
+  attr_reader :player_board,
+              :computer_board,
+              :computer_cruiser,
+              :computer_sub,
+              :player_cruiser,
+              :player_sub
+
   def initialize
-    @board = Board.new
-    # @player = Player.new
+    @player_board = Board.new
+    @computer_board = Board.new
+    @computer_cruiser = Ship.new('Cruiser', 3)
+    @computer_sub = Ship.new('Submarine', 2)
+    @player_cruiser = Ship.new('Cruiser', 3)
+    @player_sub = Ship.new('Submarine', 2)
   end
 
   def start
     puts '=== Welcome to Battleship ==='
     puts 'Instructions: ...' # Provide game instructions
-
-    setup_ships
-    play_game
+    place_cruiser
+    place_sub
   end
 
-  private
-
-  def setup_ships
+  def place_cruiser
     # Prompt the user to place their ships on the board
     puts 'Please place your ships on the board...'
     # Your logic for ship placement goes here
+    puts 'The Cruiser is 3 units long.'
+    puts 'Please place the first Cruiser coordinate (ex. A1, A2, B1, B2).'
+    input1 = gets.chomp.upcase
+    puts 'Please place the second Cruiser coordinate (ex. A1, A2, B1, B2).'
+    input2 = gets.chomp.upcase
+    puts 'Please place the third Cruiser coordinate (ex. A1, A2, B1, B2).'
+    input3 = gets.chomp.upcase
+    player_cruiser_coords = [input1, input2, input3]
+    @player_board.place(@player_cruiser, player_cruiser_coords)
+    p @player_board.render(true)
+  end
+
+  def place_sub
+    # Prompt the user to place their ships on the board
+    puts 'Please place your ships on the board...'
+    # Your logic for ship placement goes here
+    puts 'The Submarine is 2 units long.'
+    puts 'Please place the first Submarine coordinate (ex. A1, A2, B1, B2).'
+    input1 = gets.chomp.upcase
+    puts 'Please place the second Submarine coordinate (ex. A1, A2, B1, B2).'
+    input2 = gets.chomp.upcase
+    player_sub_coords = [input1, input2]
+    @player_board.place(@player_sub, player_sub_coords)
+    p @player_board.render(true)
   end
 
   def play_game
     puts 'Let the game begin!'
-
-    loop do
-      # Display the board and other game information
-      @board.display
-      # Prompt the user for their next move
-      puts 'Please enter your move...'
-      move = gets.chomp.upcase
-      # Process the move and update the game state
-      break if game_over?
-      break if move == 'EXIT'
-    end
+    # loop do
+    #   # Display the board and other game information
+    #   puts @board.render
+    #   # Prompt the user for their next move
+    #   puts 'Please enter your move...'
+    #   move = gets.chomp.upcase
+    #   # Process the move and update the game state
+    #   break if game_over?
+    #   break if move == 'EXIT'
+    # end
 
     display_game_result
   end
 
   def game_over?
+    false
     # Determines if the game is over
     # Should be a boolean
   end
