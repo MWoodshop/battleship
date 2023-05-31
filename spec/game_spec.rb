017f854226c18c6c93188dec1f6ba7856d299825
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-31 10:42:42 -0600

    Add test creates player and computer ships

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe Game do
         expect(game.player_board).to be_an_instance_of(Board)
     end
 
-    
+    it "creates player and computer ships" do
+        game = Game.new
+
+        expect(game.player_cruiser).to be_an_instance_of(Ship)
+        expect(game.player_sub).to be_an_instance_of(Ship)
+        expect(game.computer_cruiser).to be_an_instance_of(Ship)
+        expect(game.computer_sub).to be_an_instance_of(Ship)
+    end
 
 end
